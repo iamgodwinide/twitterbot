7d9871d346896790328c2cf44e74e41aa1344841
Godwin Idemudia <godwinide@gmail.com>
2023-06-24 22:40:29 +0100

    new

--- bots/firsttweet.js
+++ bots/firsttweet.js
@@ -7,7 +7,9 @@ const getFirstTweets = async (usernames, botId) => {
     // Launch Puppeteer and create a new page
     console.log("Using webscraping to gather tweets..........");
     let tweets = [];
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
 
     const getTweet = async index => {
